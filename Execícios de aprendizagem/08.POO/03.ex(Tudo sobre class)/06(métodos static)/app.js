4cfe36c25299af5c936714902981c154da0696e5
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-06 23:53:32 -0300

    feat: Utilizando métodos estáticos

--- Execícios de aprendizagem/08.POO/03.ex(Tudo sobre class)/06(métodos static)/app.js
+++ Execícios de aprendizagem/08.POO/03.ex(Tudo sobre class)/06(métodos static)/app.js
@@ -1,3 +1,4 @@
+//Utilização do Método   estático no atributo
 class Pessoa{
     _nome ;
     _idade ;
@@ -12,7 +13,7 @@ class Pessoa{
         this._peso=peso ;
         this._altura=altura ;
         this._imc=this._peso/(this._altura*this._altura); 
-        static 
+        Pessoa.totalPessoas+=1;//Adicionar +1 no totalPessoas
     }
     calculaImc(){
         return ( this._peso/(this._altura*this._altura)).toFixed(2)
@@ -22,6 +23,9 @@ class Pessoa{
     };
     get idade(){
         return this._idade
+    };
+    get totalPessoas(){
+        return this.totalPessoas
     };
     set nome(novoNome){
          this._nome=novoNome
@@ -34,6 +38,7 @@ class Pessoa{
     let pessoa1=new Pessoa('Edson ',25,68,1.7);
     let pessoa2=new Pessoa('Rodrigo',45,70,1.86);
 
+    console.log(Pessoa.totalPessoas);
     console.log("Nome" +':' + pessoa1.nome);
     console.log("Idade" +':' + pessoa1.idade);
 
