5021b0ed577ed9c9333a1829531a48e2309e46f8
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-06 23:55:21 -0300

    feat:Utilizando static ao método classificaImc

--- Execícios de aprendizagem/08.POO/03.ex(Tudo sobre class)/07(métodos classiFicalmc())/app.js
+++ Execícios de aprendizagem/08.POO/03.ex(Tudo sobre class)/07(métodos classiFicalmc())/app.js
@@ -15,24 +15,51 @@ class Pessoa{
         static 
     }
     calculaImc(){
-        return ( this._peso/(this._altura*this._altura)).toFixed(2)
+        return ( this._peso/(this._altura*this._altura)).toFixed(2);
     }
+    classificaImc(){
+        let valorImc=(this.imc).toFixed(2)
+        let classificaImc='';
+
+        if(valorImc<18.5){
+            classificaImc='Abaixo do peso'
+        }else if(valorImc<=24.9 ){
+            classificaImc='Peso normal'
+        }else if(valorImc<-29.9){
+            classificaImc='Sobrepeso'
+        }else if(valorImc<=34.9){
+            classificaImc='Obesidade grau I'
+        }else if(valorImc<=39.9){
+            classificaImc='Obesidade grau II'
+        }else{
+            classificaImc='Peso inválido'
+        }
+        return classificaImc;
+    };
     get nome(){
-        return this._nome
+        return this._nome;
     };
     get idade(){
-        return this._idade
+        return this._idade;
+    };
+    get imc(){
+        return this._imc;
     };
     set nome(novoNome){
-         this._nome=novoNome
+         this._nome=novoNome;
     };
     set idade(novoIdade){
-        this._idade=novoIdade
-   }
-    };
+        this._idade=novoIdade;
+   };
+};
     
-    let pessoa1=new Pessoa('Edson ',25,68,1.7);
+    let pessoa1=new Pessoa('Edson ',25,68,1.7,);
     let pessoa2=new Pessoa('Rodrigo',45,70,1.86);
+
+    console.log(Pessoa.totalPessoas);
+
+    console.log("Situação"+ pessoa1.classificaImc());
+    console.log("Situação"+ pessoa2.classificaImc());
 
     console.log("Nome" +':' + pessoa1.nome);
     console.log("Idade" +':' + pessoa1.idade);
