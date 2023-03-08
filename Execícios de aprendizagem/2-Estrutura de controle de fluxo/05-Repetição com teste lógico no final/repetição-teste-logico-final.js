a7d8a706e70278f39c3ecc98be8c39335f30ddc0
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-08 18:18:00 -0300

    feat:Criado laço de repetição

---
+++ Execícios de aprendizagem/2-Estrutura de controle de fluxo/05-Repetição com teste lógico no final/repetição-teste-logico-final.js
@@ -0,0 +1,13 @@
+// do(fazer)...while(enquanto)   teste lógico no final 
+// O laço do...while é semelhante ao while, mas executa o bloco de código pelo menos uma vez, antes de verificar se a condição é verdadeira.
+// Sintaxe:
+// do {
+//    bloco de código a ser executado pelo menos uma vez
+// } while (condição);
+
+//ex
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
