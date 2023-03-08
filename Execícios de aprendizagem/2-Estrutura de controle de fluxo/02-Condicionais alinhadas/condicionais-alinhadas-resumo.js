0fd44e0e0e91f57d0bc626e799958a8e1a2b0c19
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-08 17:13:34 -0300

    feat: Condicionais alinhadas resumo

---
+++ Execícios de aprendizagem/2-Estrutura de controle de fluxo/02-Condicionais alinhadas/condicionais-alinhadas-resumo.js
@@ -0,0 +1,20 @@
+// if...else if...else
+// O if...else if...else é uma estrutura condicional que permite testar várias condições em sequência e executar diferentes blocos de código, dependendo do resultado de cada teste.
+// Sintaxe:
+// if (condição1) {
+//      bloco de código a ser executado se a condição1 for verdadeira
+//   } else if (condição2) {
+//      bloco de código a ser executado se a condição1 for falsa e a condição2 for verdadeira
+//   } else {
+//      bloco de código a ser executado se todas as condições forem falsas
+//   }
+  
+//ex
+let x = 10;
+if (x > 10) {
+  console.log("x é maior do que 10");
+} else if (x < 10) {
+  console.log("x é menor do que 10");
+} else {
+  console.log("x é igual a 10");
+}
