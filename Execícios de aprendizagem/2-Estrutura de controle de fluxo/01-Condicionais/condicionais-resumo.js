083d132eeae960f3ee76063476d1cfc53d347335
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-08 17:09:07 -0300

    feat : Criado resumo sobre condicionais

---
+++ Execícios de aprendizagem/2-Estrutura de controle de fluxo/01-Condicionais/condicionais-resumo.js
@@ -0,0 +1,31 @@
+// if (se)
+// O if é a estrutura condicional mais básica em JavaScript. Ela executa um bloco de código somente se a condição especificada for verdadeira.
+// Sintaxe:
+// if (condição) {
+  // bloco de código a ser executado se a condição for verdadeira
+//}
+
+//ex
+let x = 10;
+if (x > 5) {
+  console.log("x é maior do que 5");
+};
+
+
+// if(se)...else(Senão)  
+// O if...else é uma estrutura condicional que permite executar um bloco de código se a condição especificada for verdadeira e outro bloco de código se a condição for falsa.
+// Sintaxe:
+// if (condição) {
+//   // bloco de código a ser executado se a condição for verdadeira
+// } else {
+//   // bloco de código a ser executado se a condição for falsa
+// }
+
+//ex
+let y = 10;
+if (y > 5) {
+  console.log("y é maior do que 5");
+} else {
+  console.log("y é menor ou igual a 5");
+};
+
