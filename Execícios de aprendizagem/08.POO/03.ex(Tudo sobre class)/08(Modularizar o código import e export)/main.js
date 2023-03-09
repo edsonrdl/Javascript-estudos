92d3704fdf370a986a54978b1b7fab43d1000562
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-09 00:21:56 -0300

    feat:Melhorado o código

--- Execícios de aprendizagem/08.POO/03.ex(Tudo sobre class)/08(Modularizar o código import e export)/main.js
+++ Execícios de aprendizagem/08.POO/03.ex(Tudo sobre class)/08(Modularizar o código import e export)/main.js
@@ -3,16 +3,14 @@ import {Pessoa} from "./Pessoa.js";
     let pessoa1=new Pessoa('Edson ',25,68,1.7,);
     let pessoa2=new Pessoa('Rodrigo',45,70,1.86);
 
-    console.log(Pessoa.totalPessoas);
-
-    console.log("Situação"+ pessoa1.classificaImc());
-    console.log("Situação"+ pessoa2.classificaImc());
+    console.log(`O total de Pessoas é : ${Pessoa.totalPessoas}`);
 
     console.log("Nome" +':' + pessoa1.nome);
     console.log("Idade" +':' + pessoa1.idade);
+    console.log(`Situação de ${pessoa1._nome} ${pessoa1.classificaImc()}`);
 
-    pessoa1.nome ='Edson Lopes';
-    pessoa1.idade = 89;
+    console.log("Nome" +':' + pessoa2.nome);
+    console.log("Idade" +':' + pessoa2.idade);
+    console.log(`Situação de ${pessoa2._nome} ${pessoa2.classificaImc()}`);
 
-    console.log("Nome" +':' + pessoa1.nome);
-    console.log("Idade" +':' + pessoa1.idade);
+ 
