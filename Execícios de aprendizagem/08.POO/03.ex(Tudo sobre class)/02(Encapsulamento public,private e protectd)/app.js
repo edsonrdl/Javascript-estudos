c924a81ac8ed114385a461c1bea69aa9e85012dc
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-08 23:48:11 -0300

    feat:Encapsulamento public,privado e protectd

--- Execícios de aprendizagem/08.POO/03.ex(Tudo sobre class)/02(Encapsulamento public,private e protectd)/app.js
+++ Execícios de aprendizagem/08.POO/03.ex(Tudo sobre class)/02(Encapsulamento public,private e protectd)/app.js
@@ -1,16 +1,21 @@
-//Método construtor
+//Encapsulamento public,privado e protectd
+//Usar o (_propriedade)ou(#propriedade) na frente da propriedade para dizer que é privado
+class Pessoa{
+    _nome 
+    _idade 
+    _peso 
+    };
 
- class Pessoa{
-     _nome 
-     _idade 
-     _peso
-     constructor(nome,idade,peso){
-         this._nome=nome
-         this._idade=idade 
-         this._peso=peso 
-     }
-     };  
-     let pessoa1=new Pessoa('Edson ',25,68);
-     let pessoa2=new Pessoa('Rodrigo',45,70);
-     console.log(pessoa1)
-     console.log(pessoa2)
+    let pessoa1=new Pessoa();//novo objeto pessa1
+    let pessoa2=new Pessoa();//novo objeto pessoa2
+   
+    pessoa1._nome='Edson';//passando dados para os atributo
+    pessoa1._idade=66;
+    pessoa1._peso=76;
+   
+    pessoa2._nome='Rodrigo';//passando dados para os atributo
+     pessoa2._idade=65;
+    pessoa2._peso=70;
+    
+   console.log(pessoa1);
+   console.log(pessoa2);
