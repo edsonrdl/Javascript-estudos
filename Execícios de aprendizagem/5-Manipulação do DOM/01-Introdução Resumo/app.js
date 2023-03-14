7ab0032726bfe56b9e7d136510f6316edc4c29bb
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-14 18:30:45 -0300

    feat: Dom

---
+++ Execícios de aprendizagem/5-Manipulação do DOM/01-Introdução Resumo/app.js
@@ -0,0 +1,32 @@
+
+// O que é o Dom ? "document object model"
+// visualizando o dom 
+// ## eventos
+// ## Criação de elementos
+// ## .cloneNode()
+// ##ClassList
+// ##Target
+
+// const form=document.querySelector('form'); //Usar se tiver apenas um evento de submit considerando o ultimo criado 
+// form.onsubmit=function(event){
+//     event.preventDefault();
+// }
+// form.addEventListener('submite',function(event){
+//     event.preventDefault();
+
+// })//pode ter quantos quise
+// Por marca getElementsbytagname()[
+//por id getElementbyId()
+//por class getElementclassName()
+//por nome getElementsByName()
+//Por seletor querySelector()
+//Por seletor querySelectorAll()
+
+
+form.addEventListener('submite',function(event){
+    event.preventDefault();
+    const input=inout.form.querySelector('input');
+    const value=inoyut.value
+
+    const li=document.createElement('li')
+});
