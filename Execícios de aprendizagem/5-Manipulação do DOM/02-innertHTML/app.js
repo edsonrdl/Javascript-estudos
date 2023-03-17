d3b3a86f22b2379ae2b648e2cb01f9b8bd222f90
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-17 16:22:06 -0300

    feat:Criando exercicios

--- Execícios de aprendizagem/5-Manipulação do DOM/02-innertHTML/app.js
+++ Execícios de aprendizagem/5-Manipulação do DOM/02-innertHTML/app.js
@@ -1,4 +1,5 @@
 
-let corteste=document.getElementById('teste');
-console.log(corteste);
-corteste.innerHTML='Edson'; //ou corteste.textContent='Edson';
+const itensNav=document.getElementsByClassName('teste-item');
+let qtdItens=itensNav.length
+console.log(qtdItens)
+const newItens=itensNav.splice()
