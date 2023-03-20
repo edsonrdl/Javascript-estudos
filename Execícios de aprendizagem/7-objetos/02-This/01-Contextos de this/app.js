ad4254c5a549d853a80c9e6570a5ddb4f17a1994
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-20 18:32:19 -0300

    feat: Corrigido

--- Execícios de aprendizagem/7-objetos/02-This/01-Contextos de this/app.js
+++ Execícios de aprendizagem/7-objetos/02-This/01-Contextos de this/app.js
@@ -1,3 +1,12 @@
-let pessoas={
+ //Não vai funcionar por não ter ainda o firstName então na hora de chamar vai dar (undefined)
+ const user={
+    info(){
+        return "My name is "+ this.firstName;
 
-}
+},
+ } 
+ console.log(user.info()) //o this é o user nesse contexto
+// function infor(){
+//     return  "My name is"+ this.firstName;
+// };
+//  infor(); //Nada esta chamando o infor para o this fazer parte do contexto
