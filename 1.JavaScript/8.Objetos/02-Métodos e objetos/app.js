262de66cf5ee9989b6fa022b70c1c78f4480e227
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-05-13 13:06:01 -0300

    feat: criado métodos de objetos

--- 1.JavaScript/8.Objetos/02-Métodos e objetos/app.js
+++ 1.JavaScript/8.Objetos/02-Métodos e objetos/app.js
@@ -1,45 +1,27 @@
-//Usa a mesma função duplicada em cada objeto 
-// var pessoa={
-//     nome:'Rodrigo',
-//     sobrenome:'lima',
-//     idade:'29',
-//     getNome:function(){
-//         this.nome;
 
-//     }
-// };
-// var carros={
-//     nome:'Gol',
-//     sobrenome:'VW',
-//     getNome:function(){
-//         this.nome;
-//     }
-// }
-//ou  essa outra forma para usar apenas uma função e usar nos dois objetos 
-var pegarNome = function(nome,sobrenome){
-    this.nome=nome;
-    this.sobrenome=sobrenome;
-    console.log(this)
-}//se não for colocado na função não vai ser alterado lá na frente utilizando o call
+var pegarNome = function(nome){
+ return this.nome;
+};
 
 var pessoa={
     nome:'Rodrigo',
     sobrenome:'lima',
     idade:'29',
-    getNome:pegarNome //referência para a função 
+    getNome:pegarNome
 };
 
-var carros={
+var carro={
     nome:'Gol',
     marca:'VW',
-    getNome:pegarNome//referência para a função 
+    getNome:pegarNome
 };
 
-pessoa.getNome();
-carros.getNome();
 
-pegarNome.call(pessoa,'Pedro','Rocha');
-//OU 
-pegarNome.apply(pessoa,['Pedro','Rocha']);
-//OU
-pegarNome.bind(pessoa,'Pedro','Rocha');
+
+var teste1=pessoa.getNome();
+console.log(teste1);
+
+var teste2=carro.getNome();
+console.log(teste2);
+
+
