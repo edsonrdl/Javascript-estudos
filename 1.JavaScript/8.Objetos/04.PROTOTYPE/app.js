743c8c2d2af480837893ac94da0ba22416591c47
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-05-13 20:06:39 -0300

    feat: corrigido err

--- 1.JavaScript/8.Objetos/04.PROTOTYPE/app.js
+++ 1.JavaScript/8.Objetos/04.PROTOTYPE/app.js
@@ -1,43 +1,22 @@
-function Pessoa(nome,idade,sexo,cabelo){
-     this._nome=this.nome;
-     this.idade=this.idade;
-     this.sexo=this.sexo;
- }
- Pessoa.prototype.getNome=function(){
-    return this.nome;
- }
- Pessoa.prototype.getIdade=function(){
-    return this.idade;
- }
-
-var rodrigo=new Pessoa('Rodrigo',21,'M');
- var joao=new Pessoa('João',19,'M');
-
-console.log(rodrigo.getNome(),rodrigo.getIdade())
-console.log(joao.getNome(),joao.getIdade())
-
 //outra maneira sem ter que ficar alterando os nomes
-function Pessoa(nome,idade,sexo,cabelo){
-    this._nome=this.nome;
-    this.idade=this.idade;
-    this.sexo=this.sexo;
+function Pessoa(nome,idade,sexo){
+    this._nome=nome;
+    this._idade=idade;
+    this._sexo=sexo;
 }
 Pessoa.prototype={
     constructor:Pessoa,//Para definir que realmente é instância
     getNome:function(){
-        return this.nome;
-    }
-},
-Pessoa.prototype={
+        return this._nome;
+    },
     getIdade:function(){
-        return this.idade;
-    }
+    return this._idade;
 },
-Pessoa.prototype={
+
     getSexo:function(){
-        return this.sexo;
-    }
+    return this._sexo; 
 }
+};
 
 var rodrigo=new Pessoa('Rodrigo',21,'M');
 var joao=new Pessoa('João',19,'M');
