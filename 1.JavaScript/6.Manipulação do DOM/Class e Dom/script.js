0cc34dc4421bb48758b104cbafdf3b0833c26f75
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-06-29 12:55:36 -0300

    feat:atualizando

--- 1.JavaScript/6.Manipulação do DOM/Class e Dom/script.js
+++ 1.JavaScript/6.Manipulação do DOM/Class e Dom/script.js
@@ -1,25 +1,3 @@
-//Exemplos simples 
-// class Menu {
-//   constructor(menu) {
-//     this.menuElement = document.querySelector(menu);
-//     this.activeClass = "active";
-//   }
-//   addActiveEvent() {
-//     this.menuElement.addEventListener("click", event =>
-//       event.target.classList.add(this.activeClass)
-//     );
-//   }
-// }
-// // teste
-// const menu = new Menu(".principal");
-// menu.addActiveEvent();
-
-// const upperName = name => name.toUpperCase();
-// const countLetters = word => word.length;
-
-// console.log(countLetters("Andre"));
-
-//exemplo utilizando class e Dom
 class Menu {
   constructor(menu) {
     this.menuElement = document.querySelector(menu);
@@ -39,13 +17,37 @@ menu.addActiveEvent();
 
 //Outro exemplo para filtras usando arrow function
 let produtos=[
-  {produto:'camisa',preco:25,cor:'amarelo'},
-  {produto:'sandalia',preco:67,cor:'rox'},
-  {produto:'bermuda',preco:23,cor:'vermelho'},
-  {produto:'bone',preco:10,cor:'rosa'},
-  {produto:'short',preco:80,cor:'verde'},
-  {produto:'oculos',preco:45,cor:'azul'},
-  {produto:'camiseta',preco:50,cor:'preto'},
+  {
+    produto:'camisa',
+    preco:25,
+    cor:'amarelo'},
+  {
+    produto:'sandalia'
+    ,preco:67,cor:'rox'},
+  {
+    produto:'bermuda',
+    preco:23,
+    cor:'vermelho'},
+  {
+    produto:'bone'
+    ,preco:10,
+    cor:'rosa'
+  },
+  {
+    produto:'short',
+    preco:80,
+    cor:'verde'
+  },
+  {
+    produto:'oculos'
+    ,preco:45,
+    cor:'azul'
+  },
+  {
+    produto:'camiseta'
+    ,preco:50,
+    cor:'preto'
+  },
 ];
 let precoMenorQue50=produtos.filter((produtos)=>{
   return produtos.preco<=50;
