9c71001406857a943a5057e669ef6070101efbff
SEFA-PA\edson.lopes <edson.lopes@sefa.pa.gov.br>
2025-05-09 14:05:35 -0300

    atualizado

--- 1.JavaScript/1.Váriaveis e tipos de dados/1.1. variável/script.js
+++ 1.JavaScript/1.Váriaveis e tipos de dados/1.1. variável/script.js
@@ -1,6 +1,6 @@
 // Utilização do var para criar uma variável na memória e utilizar
 
-// Declacação de variável em javascript antes do ES6
+// Declaração de variável em javascript antes do ES6
 
 var nome = 'João';
 
